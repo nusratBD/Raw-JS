7a194a3e60b8fce619a8395ed86b4a234eb06695
nusratBD <nusrat.hurain15@gmail.com>
2025-05-04 23:19:55 +0600

    Bubble sort & Event Deligate

---
+++ bubble-sort.js
@@ -0,0 +1,9 @@
+document.getElementById("item-3").addEventListener("click",(event)=>{
+    console.log("Item-3 Clicked");
+})
+document.getElementById("item-4").addEventListener("click",(event)=>{
+    console.log("Item-4 Clicked");
+})
+document.getElementById("ui-item").addEventListener("click",()=>{
+    console.log("UI Clicked");
+})
