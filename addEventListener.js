da2f229240d900b4047fa2d7f861ea9c02e231f5
nusratBD <nusrat.hurain15@gmail.com>
2025-05-04 21:54:30 +0600

    onfocus, set & remove attribute in JS

--- addEventListener.js
+++ addEventListener.js
@@ -1,9 +1,19 @@
+const postArea=document.getElementById("post");
+const postInput=document.getElementById("post-input");
+const deleteInput=document.getElementById("delete");
+const deleteBtn=document.getElementById("delete-btn");
 document.getElementById("eventBtn").addEventListener("click",()=>{
-    const postArea=document.getElementById("post");
-    const postInput=document.getElementById("post-input");
     let postValue=postInput.value;
     postArea.innerHTML+=`
     <p>${postValue}</p>
     `;
     postInput.value="";
 })
+const deleteHandle=()=>{
+    const deleteText=deleteInput.value.trim();
+    if(deleteText=="Delete"){
+        deleteBtn.removeAttribute("disabled", false);
+    }else{
+        deleteBtn.setAttribute("disabled", true);
+    }
+}
